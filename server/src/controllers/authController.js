4ab4ddc07f38baa4782d0d158163b59cd3b484d7
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-04-25 10:11:20 +0300

    Add authController

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,4 +1,17 @@
-const { User } = require('../models');
+const { promisify } = require('util');
+const createHttpError = require('http-errors');
+const jwt = require('jsonwebtoken');
+const { User, RefreshToken } = require('../models');
+const {
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_TIME,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_TIME,
+  MAX_DEVICE_AMOUNT,
+} = require('../constants');
+
+const signJWT = promisify(jwt.sign);
+
 module.exports.signIn = async (req, res, next) => {
   try {
     const {
@@ -8,16 +21,47 @@ module.exports.signIn = async (req, res, next) => {
     const user = await User.findOne({
       where: { email },
     });
-
     //if found - compare password
-    if(user && (await user.comparePassword(password))){
-    //create pair tokens
-    //send user with pair tokens      
-      res.status(200).send()
+    if (user && (await user.comparePassword(password))) {
+      //create pair tokens
+      const accessToken = await signJWT(
+        {
+          userId: user.id,
+          email: user.email,
+          role: user.role,
+        },
+        ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: ACCESS_TOKEN_TIME,
+        }
+      );
+      const refreshToken = await signJWT(
+        {
+          userId: user.id,
+          email: user.email,
+          role: user.role,
+        },
+        REFRESH_TOKEN_SECRET,
+        {
+          expiresIn: REFRESH_TOKEN_TIME,
+        }
+      );
+      // 3 device
+      if ((await user.countRefreshTokens()) > MAX_DEVICE_AMOUNT) {
+        const [oldestToken] = await user.getRefreshTokens({
+          order: [['createdAt', 'DESC']],
+        });
+        await oldestToken.update({ value: refreshToken });
+      } else {
+        await user.createRefreshToken({ value: refreshToken });
+      }
+      user.password = undefined;
+      //send user with pair tokens
+      res
+        .status(200)
+        .send({ data: { user, tokenPair: { accessToken, refreshToken } } });
     }
-    //if not found - error
-    //!!!!! createError
-    res.status(409).send() 
+    next(createHttpError(401, 'Invalid credentials!'));
   } catch (error) {
     next(error);
   }
@@ -25,7 +69,48 @@ module.exports.signIn = async (req, res, next) => {
 
 module.exports.signUp = async (req, res, next) => {
   try {
-    await User.create()
+    const {body} = req;
+    const user = await User.create(body);
+    if (user) {
+      //create pair tokens
+      const accessToken = await signJWT(
+        {
+          userId: user.id,
+          email: user.email,
+          role: user.role,
+        },
+        ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: ACCESS_TOKEN_TIME,
+        }
+      );
+      const refreshToken = await signJWT(
+        {
+          userId: user.id,
+          email: user.email,
+          role: user.role,
+        },
+        REFRESH_TOKEN_SECRET,
+        {
+          expiresIn: REFRESH_TOKEN_TIME,
+        }
+      );
+      // 3 device
+      if ((await user.countRefreshTokens()) > MAX_DEVICE_AMOUNT) {
+        const [oldestToken] = await user.getRefreshTokens({
+          order: [['createdAt', 'DESC']],
+        });
+        await oldestToken.update({ value: refreshToken });
+      } else {
+        await user.createRefreshToken({ value: refreshToken });
+      }
+      user.password = undefined;
+      //send user with pair tokens
+      res
+        .status(201)
+        .send({ data: { user, tokenPair: { accessToken, refreshToken } } });
+    }
+    next(createHttpError(400, 'Bad request!'));
   } catch (error) {
     next(error);
   }
@@ -33,6 +118,40 @@ module.exports.signUp = async (req, res, next) => {
 
 module.exports.refresh = async (req, res, next) => {
   try {
+    const {body: {refreshToken}} = req;
+    const refreshTokenInstance = await RefreshToken.findOne({
+      where: {value: refreshToken}
+    })
+    const user = await refreshTokenInstance.getUser();
+    // const user = await User.findOne({
+    //   where: {id: refreshTokenInstance.userId}
+    // })
+    const accessToken = await signJWT(
+      {
+        userId: user.id,
+        email: user.email,
+        role: user.role,
+      },
+      ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: ACCESS_TOKEN_TIME,
+      }
+    );
+    const refreshTokenNew = await signJWT(
+      {
+        userId: user.id,
+        email: user.email,
+        role: user.role,
+      },
+      REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: REFRESH_TOKEN_TIME,
+      }
+    );
+    await refreshTokenInstance.update({ value: refreshTokenNew });
+    res
+        .status(200)
+        .send({ data: { user, tokenPair: { accessToken, refreshTokenNew } } });
   } catch (error) {
     next(error);
   }
